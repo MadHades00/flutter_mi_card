c78175097ae73cd2bb147db09fa2815307e7d892
MadHades00 <nicolas.gauthe@outlook.com>
2022-01-07 15:42:07 +0100

    Row and Column course

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,15 +11,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello !'),
-          ),
-        ),
+            child: Column(
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+          ],
+        )),
       ),
     );
   }
